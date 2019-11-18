489137a2bcc32dc6405c0996472502ed708774ed
고정섭 <mayaul@woowahan.com>
2019-11-18 12:47:20 +0900

    20191108

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Content from './Components/Content'
+import Subject from './Components/Subject'
+import Navigation from './Components/Navigation'
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1><a href="" onClick={function(e) { alert('click'); e.preventDefault(); }}>click</a></h1>
+        </header>
+        <Subject title="Web" sub="world wide web!!22233"></Subject>
+        <Navigation></Navigation>
+        <Content></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Content.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Content extends Component {
+	render() {
+        return (
+            <article>
+                <h2>React App</h2>
+                study!!!
+            </article>
+        );
+	}
+}
+
+export default Content
