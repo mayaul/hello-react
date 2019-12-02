79b6bd962a68526c6debc03e0b9aa77f86098722
고정섭 <mayaul@woowahan.com>
2019-12-02 13:06:56 +0900

    191202

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navigation from './Components/Navigation'
 import Control from './Components/Control'
 import ReadContent from './Components/ReadContent'
 import CreateContent from "./Components/CreateContent";
+import UpdateContent from "./Components/UpdateContent";
 
 class App extends Component {
   constructor(props) {
@@ -22,44 +23,53 @@ class App extends Component {
       ]
     }
   }
-  render() {
-    console.log("app render");
+  getReadContent() {
+    var i = 0;
 
+    while(i < this.state.contents.length) {
+      var data = this.state.contents[i];
+
+      if (data.id === this.state.selected_content_id) {
+        return data;
+      }
+
+      i++;
+    }
+  }
+  getContent() {
     var _title, _desc, _article = null;
+    var _content = this.getReadContent();
 
     if (this.state.mode === 'welcome') {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.sub;
       _article = <ReadContent title={_title} sub={_desc}></ReadContent>
     } else if (this.state.mode === 'read') {
-      var i = 0;
-
-      while(i < this.state.contents.length) {
-        var data = this.state.contents[i];
-
-        if (data.id === this.state.selected_content_id) {
-
-          _title = data.title;
-          _desc = data.desc;  
-          break;
-        }
-
-        i++;
-      }
-      _article = <ReadContent title={_title} sub={_desc}></ReadContent>
+      _article = <ReadContent title={_content._title} sub={_content._desc}></ReadContent>
     } else if (this.state.mode === 'create') {
       _article = <CreateContent onSubmit={function (_title, _desc) {
         this.max_content_id = this.max_content_id + 1;
         var _contents = this.state.contents.concat({id:this.max_content_id, title:_title, desc: _desc});
         this.setState({contents: _contents})
       }.bind(this)}></CreateContent>
+    } else if (this.state.mode === 'update') {
+      _article = <UpdateContent data={_content} onSubmit={function (_title, _desc) {
+        this.max_content_id = this.max_content_id + 1;
+        var _contents = this.state.contents.concat({id:this.max_content_id, title:_title, desc: _desc});
+        this.setState({contents: _contents})
+      }.bind(this)}></UpdateContent>
     }
+    return _article;
+  }
+  render() {
+    console.log("app render");
+
     return (
       <div className="App">
         <Subject title={this.state.subject.title} sub={this.state.subject.sub} onChangePage={function() { this.setState({mode:'read'}) }.bind(this)}></Subject>
         <Navigation data={this.state.contents} onChangePage={function(id) { this.setState({selected_content_id:Number(id)}) }.bind(this)}></Navigation>
         <Control onChangeMode={function(mode) { this.setState({mode: mode})}.bind(this)}></Control>
-        {_article}
+        {this.getContent()}
       </div>
     );
   }
